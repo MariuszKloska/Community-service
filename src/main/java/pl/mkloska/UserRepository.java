cba91842ea79646d95722398931d116b90ecde43
MariuszKloska <mariuszkloska@poczta.fm>
2022-11-16 22:37:34 +0100

    basic functionality done

--- src/main/java/pl/mkloska/UserRepository.java
+++ src/main/java/pl/mkloska/UserRepository.java
@@ -2,5 +2,9 @@ package pl.mkloska;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/pl/mkloska/UserService.java
+++ src/main/java/pl/mkloska/UserService.java
@@ -3,6 +3,7 @@ package pl.mkloska;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserService {
@@ -26,6 +28,12 @@ public class UserService {
     }
     @PostMapping("/users")
     public ResponseEntity addUser(@RequestBody User user) {
+        Optional<User> userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if(userFromDb.isPresent()){
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
+
         User savedUser = userRepository.save(user);
         return ResponseEntity.ok(savedUser);
     }
